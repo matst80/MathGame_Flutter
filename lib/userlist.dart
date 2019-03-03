ad9848d0090a1cd2155802c648317683d0a6bedf
Mats Törnberg <mats.tornberg@gmail.com>
2019-03-03 11:56:11 +0100

    new userlist

--- lib/userlist.dart
+++ lib/userlist.dart
@@ -15,30 +15,60 @@ var userNameStyle = TextStyle(
   fontWeight: FontWeight.bold,
 );
 
+// class _UserList extends State<UserList> {
+//   @override
+//   Widget build(BuildContext context) {
+//     var userWidgets = widget.users
+//         .map((user) => Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children: [
+//                 Padding(
+//                   padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+//                   child: Text(
+//                     user.name,
+//                     style: userNameStyle,
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+//                   child: Chip(
+//                     backgroundColor: Colors.black,
+//                     labelStyle: TextStyle(color: Colors.white),
+//                     label: Text(user.score.toString()),
+//                   ),
+//                 ),
+//               ],
+//             ))
+//         .toList();
+//     return Column(
+//       mainAxisAlignment: MainAxisAlignment.start,
+//       children: userWidgets,
+//     );
+//   }
+// }
+
 class _UserList extends State<UserList> {
   @override
   Widget build(BuildContext context) {
-    var userWidgets = widget.users
-        .map((user) => Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal:16, vertical: 4),
-                  child: Text(
-                    user.name,
-                    style: userNameStyle,
+    var users = widget.users;
+    users.sort((a, b) => a.score - b.score);
+    var userWidgets = users
+        .map(
+          (user) => Chip(
+                backgroundColor: Colors.black87,
+                labelStyle: TextStyle(color: Colors.white),
+                label: Text.rich(
+                  TextSpan(
+                    text: user.name, // default text style
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: ' ' + user.score.toString(),
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    ],
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal:16, vertical: 4),
-                  child: Chip(
-                    backgroundColor: Colors.black,
-                    labelStyle: TextStyle(color: Colors.white),
-                    label: Text(user.score.toString()),
-                  ),
-                ),
-              ],
-            ))
+              ),
+        )
         .toList();
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
